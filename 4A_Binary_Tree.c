b92f3772873027bb9ab234b18ac7910880aa9737
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2022-12-23 23:45:31 +0530

    new program

--- 4A_Binary_Tree.c
+++ 4A_Binary_Tree.c
@@ -1,55 +1,78 @@
 /*
     QUESTION :
-        Design, Develop and Implement a menu driven Program in C for the following operations on
-        Binary Search Tree (BST) of Integers
-            a. Create a BST of N Integers
-            b. Traverse the BST in Inorder, Preorder and Post Order
-
+        Given an array of elements, construct a complete binary tree from this array in level order
+        fashion. That is, elements from left in the array will be filled in the tree level wise starting from
+        level 0. Ex: Input :
+        arr[] = {1, 2, 3, 4, 5, 6}
+        Output : Root of the following tree
+                1
+               / \
+              2   3
+             / \ / \
+            4    5  6
     OUTPUT :
         MENU - 1:Insert, 2:Preorder, 3:Inorder, 4:Postorder, 5:Exit
         Enter your choice : 1
         Enter element to insert : 10
+        
         MENU - 1:Insert, 2:Preorder, 3:Inorder, 4:Postorder, 5:Exit
         Enter your choice : 1
         Enter element to insert : 5
+        Enter the direction to insert (say 'l' or 'r') : l
+        
         MENU - 1:Insert, 2:Preorder, 3:Inorder, 4:Postorder, 5:Exit
         Enter your choice : 1
         Enter element to insert : 3
+        Enter the direction to insert (say 'l' or 'r') : ll
+        
         MENU - 1:Insert, 2:Preorder, 3:Inorder, 4:Postorder, 5:Exit
         Enter your choice : 1
         Enter element to insert : 2
+        Enter the direction to insert (say 'l' or 'r') : lll
+        
         MENU - 1:Insert, 2:Preorder, 3:Inorder, 4:Postorder, 5:Exit
         Enter your choice : 1
         Enter element to insert : 6
+        Enter the direction to insert (say 'l' or 'r') : lr
+        
         MENU - 1:Insert, 2:Preorder, 3:Inorder, 4:Postorder, 5:Exit
         Enter your choice : 1
         Enter element to insert : 15
+        Enter the direction to insert (say 'l' or 'r') : r
+        
         MENU - 1:Insert, 2:Preorder, 3:Inorder, 4:Postorder, 5:Exit
         Enter your choice : 1
         Enter element to insert : 12
+        Enter the direction to insert (say 'l' or 'r') : rl
+        
         MENU - 1:Insert, 2:Preorder, 3:Inorder, 4:Postorder, 5:Exit
         Enter your choice : 1
         Enter element to insert : 20
+        Enter the direction to insert (say 'l' or 'r') : rr
+        
         MENU - 1:Insert, 2:Preorder, 3:Inorder, 4:Postorder, 5:Exit
         Enter your choice : 1
         Enter element to insert : 25
+        Enter the direction to insert (say 'l' or 'r') : rrr
+        
         MENU - 1:Insert, 2:Preorder, 3:Inorder, 4:Postorder, 5:Exit
         Enter your choice : 2
         
-        Preorder : 10	5	3	2	6	15	12	20	25
+        Preorder : 10   5       3       2       6       15      12      20      25
         MENU - 1:Insert, 2:Preorder, 3:Inorder, 4:Postorder, 5:Exit
         Enter your choice : 3
         
-        Inorder : 2    3	5	6	10	12	15	20	25
+        Inorder : 2     3       5       6       10      12      15      20      25
         MENU - 1:Insert, 2:Preorder, 3:Inorder, 4:Postorder, 5:Exit
         Enter your choice : 4
         
-        Postorder : 2	3	6	5	12	25	20	15	10
+        Postorder : 2   3       6       5       12      25      20      15      10
         MENU - 1:Insert, 2:Preorder, 3:Inorder, 4:Postorder, 5:Exit
         Enter your choice : 5
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define pf printf
 #define sf scanf
@@ -96,15 +119,18 @@ NODE getnode()
     NODE x = (NODE)malloc(sizeof(struct bt));
     if (x == NULL)
     {
-        pf("Malloc Failed!");
+        pf("Malloc Failed!\n");
         exit(1);
     }
     return x;
 }
 
-NODE insertbst(int item, NODE root)
+NODE insertbt(int item, NODE root)
 {
     NODE temp, cur, prev;
+    char dir[15];
+    int i;
+    cur = root;
     temp = getnode();
     temp->info = item;
     temp->ll = temp->rl = NULL;
@@ -112,14 +138,28 @@ NODE insertbst(int item, NODE root)
     {
         return temp;
     }
+    pf("Enter the direction to insert (say 'l' or 'r') : ");
+    sf("%s", dir);
     prev = NULL;
-    cur = root;
-    while (cur != NULL)
+    for (i = 0 ; ( ( i < strlen(dir) ) && ( cur != NULL ) ) ; i++)
     {
         prev = cur;
-        cur = ( (item > cur->info) ? cur->rl : cur->ll );
+        if (dir[i] == 'l')
+        {
+            cur = cur->ll;
+        }
+        else
+        {
+            cur = cur->rl;
+        }
     }
-    if (item < prev->info)
+    if ( ( cur != NULL ) || ( i != strlen(dir) ) )
+    {
+        pf("Cannot Insert!\n");
+        free(temp);
+        return root;
+    }
+    if (dir[i-1] == 'l')
     {
         prev->ll = temp;
     }
@@ -144,7 +184,7 @@ int main()
             case 1 :
                 pf("Enter element to insert : ");
                 sf("%d", &item);
-                root = insertbst(item, root);
+                root = insertbt(item, root);
                 break;
 
             case 2 :
